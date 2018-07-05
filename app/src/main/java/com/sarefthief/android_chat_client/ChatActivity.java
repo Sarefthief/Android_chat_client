b79375105bfe31754112b8e700948db5e13749ee
Sarefthief <Sarefthief@gmail.com>
2018-07-05 14:28:53 +0700

    added message class

--- app/src/main/java/com/sarefthief/android_chat_client/ChatActivity.java
+++ app/src/main/java/com/sarefthief/android_chat_client/ChatActivity.java
@@ -2,14 +2,19 @@ package com.sarefthief.android_chat_client;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class ChatActivity extends AppCompatActivity {
 
+    TextView chat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        chat = findViewById(R.id.chatView);
+        chat.setMovementMethod(new ScrollingMovementMethod());
     }
 
     public void onClick(View view)
