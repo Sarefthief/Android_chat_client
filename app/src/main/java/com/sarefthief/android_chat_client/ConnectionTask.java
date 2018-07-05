f4bed9b3764e23f957b5f5b9fbd7f50778a4b242
Sarefthief <Sarefthief@gmail.com>
2018-07-05 13:36:17 +0700

    added tasks

--- app/src/main/java/com/sarefthief/android_chat_client/ConnectionTask.java
+++ app/src/main/java/com/sarefthief/android_chat_client/ConnectionTask.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.widget.TextView;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
---
+++ app/src/main/java/com/sarefthief/android_chat_client/ReadTask.java
@@ -0,0 +1,16 @@
+package com.sarefthief.android_chat_client;
+
+import android.os.AsyncTask;
+
+public class ReadTask extends AsyncTask<Void, Void, Void>
+{
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+}
