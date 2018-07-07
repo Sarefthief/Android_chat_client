05c9a2c4009ab5e40e8a3f781424133f4505e326
Sarefthief <Sarefthief@gmail.com>
2018-07-07 14:26:55 +0700

    added messages adapter

--- app/src/main/java/com/sarefthief/android_chat_client/ChatActivity.java
+++ app/src/main/java/com/sarefthief/android_chat_client/ChatActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -13,8 +16,13 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        chat = findViewById(R.id.chatView);
-        chat.setMovementMethod(new ScrollingMovementMethod());
+        //chat = findViewById(R.id.chatView);
+        //chat.setMovementMethod(new ScrollingMovementMethod());
+
+        ArrayList<Message> arrayOfUsers = new ArrayList<Message>();
+        MessageAdapter adapter = new MessageAdapter(this, arrayOfUsers);
+        ListView listView = findViewById(R.id.messages);
+        listView.setAdapter(adapter);
     }
 
     public void onClick(View view)
---
+++ app/src/main/java/com/sarefthief/android_chat_client/MessageAdapter.java
@@ -0,0 +1,33 @@
+package com.sarefthief.android_chat_client;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MessageAdapter extends ArrayAdapter<Message> {
+
+    public MessageAdapter(Context context, ArrayList<Message> users) {
+        super(context, 0, users);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Message message = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_message, parent, false);
+        }
+        // Lookup view for data population
+        TextView tvMessage = convertView.findViewById(R.id.tvMessage);
+        // Populate the data into the template view using the data object
+        tvMessage.setText(message.getMessage());
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
